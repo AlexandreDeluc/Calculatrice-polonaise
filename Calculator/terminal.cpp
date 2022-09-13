a260bde82df3337e459eb3d66937dd4bc76050bd
williamprevot <guy-william.prevot@isen.yncrea.fr>
2022-09-13 02:58:33 +0200

    teminal.cpp

--- Calculator/terminal.cpp
+++ Calculator/terminal.cpp
@@ -88,8 +88,8 @@ vector<string> splitCalcul(string str)
 }
 
 void terminal()
-{
-	stack<Expression*> stock;
+{	stack<Expression*> stock;
+	// stack<Operateur*> stock;
 	vector<string> calcul_atoms;
 	cout << INFO_MSG << endl;
 	string ma_saisie = "";
@@ -100,107 +100,105 @@ void terminal()
 	calcul_atoms = splitCalcul(ma_saisie);
 
 
-	// while (ma_saisie != "p") {
+	while (ma_saisie != "p") {
 
-	// 	cin >> ma_saisie; //caractere qui va etre saisi et enregistre dans une pile
+		cin >> ma_saisie; //caractere qui va etre saisi et enregistre dans une pile
 
-	// 	bool digit = true;
-	// 		bool alpha = true;
-	// 		int init = 0;
+		bool digit = true;
+			bool alpha = true;
+			int init = 0;
 
-	// 		if (ma_saisie.length() > 1) {
-	// 			if ((char)ma_saisie[0] == '-') {
-	// 				init = 1;
-	// 			}
-	// 		}
+			if (ma_saisie.length() > 1) {
+				if ((char)ma_saisie[0] == '-') {
+					init = 1;
+				}
+			}
 
-	// 		for (int i = init; i <= ma_saisie.length() - 1; i++) {
-	// 			if (!isdigit(((char)ma_saisie[i]))) {
-	// 				digit = false;
-	// 			}
-	// 			else
-	// 				digit=true;
-	// 		}
+			for (int i = init; i <= ma_saisie.length() - 1; i++) {
+				if (!isdigit(((char)ma_saisie[i]))) {
+					digit = false;
+				}
+				
+			}
 
-	// 		for (int i = init; i <= ma_saisie.length() - 1; i++) {
-	// 			if (!isalpha(((char)ma_saisie[i]))) {
-	// 				alpha = false;
-	// 			}
-	// 			else
-	// 				alpha=true;
-	// 		}
+			for (int i = init; i <= ma_saisie.length() - 1; i++) {
+				if (!isalpha(((char)ma_saisie[i]))) {
+					alpha = false;
+				}
+				
+			}
 
-	// 		if (digit) { //si l'utilisateur saisit un nombre
-	// 			float saisie = stof(ma_saisie);
-	// 			Constante *e =  new Constante((float) saisie);
-	// 			stock.push(e);
-	// 		}
+			if (digit) { //si l'utilisateur saisit un nombre
+				float saisie = stof(ma_saisie);
+				Constante *e =  new Constante((float) saisie);
+				stock.push(e);
+			}
 
-	// 		else if (alpha && ma_saisie != "p") {//si l'utilisateur saisit une lettre differente de p
-	// 			cout << "Vous avez saisi une lettre." << endl;
-	// 			stock.push(ma_saisie);
-	// 		}
+			else if (alpha && ma_saisie != "p") {//si l'utilisateur saisit une lettre differente de p
+				cout << "Vous avez saisi une lettre." << endl;
+				stock.push(ma_saisie);
+			}
 
 
-	// 		else if (ma_saisie == "p") {//si l'utilisateur saisit la lettre p, qui signifie la fin de la saisie
-	// 			cout << endl << "Vous avez saisi le caractere 'p' : la saisie est finie." << endl;
+			else if (ma_saisie == "p") {//si l'utilisateur saisit la lettre p, qui signifie la fin de la saisie
+				cout << endl << "Vous avez saisi le caractere 'p' : la saisie est finie." << endl;
 
-	// 		}
-	// 		else { //si l'utilisateur saisit tout autre type de caractere
+			}
+			else { //si l'utilisateur saisit tout autre type de caractere
 
-	// 			if (ma_saisie == "+") {
-	// 				string sa = stock.top();
-	// 				stock.pop();
-	// 				string sb = stock.top();
-	// 				stock.pop();
-	// 				float a = stof(sa);
-	// 				float b = stof(sb);
-	// 				Addition* adt = new Addition(b, a);
-	// 				float c = adt->calculer();
-	// 				string sc = to_string(c);
-	// 				stock.push(sc);
-	// 			}
-	// 			else if (ma_saisie == "-") {
-	// 				string sa = stock.top();
-	// 				stock.pop();
-	// 				string sb = stock.top();
-	// 				stock.pop();
-	// 				float a = stof(sa);
-	// 				float b = stof(sb);
-	// 				Soustraction* stc = new Soustraction(b, a);
-	// 				float c = stc->calculer();
-	// 				string sc = to_string(c);
-	// 				stock.push(sc);
-	// 			}
-	// 			else if (ma_saisie == "*") {
-	// 				string sa = stock.top();
-	// 				stock.pop();
-	// 				string sb = stock.top();
-	// 				stock.pop();
-	// 				float a = stof(sa);
-	// 				float b = stof(sb);
-	// 				Multiplication* mct = new Multiplication(b, a);
-	// 				float c = mct->calculer();
-	// 				string sc = to_string(c);
-	// 				stock.push(sc);
-	// 			}
-	// 			else if (ma_saisie == "/") {
-	// 				string sa = stock.top();
-	// 				stock.pop();
-	// 				string sb = stock.top();
-	// 				stock.pop();
-	// 				float a = stof(sa);
-	// 				float b = stof(sb);
-	// 				Division* dvt = new Division(b, a);
-	// 				float c = dvt->calculer();
-	// 				string sc = to_string(c);
-	// 				stock.push(sc);
-	// 			}
-	// 			else {
-	// 				cout << "ERROR" << endl;
-	// 			}
-	// 		}
-	// 	}
+				if (ma_saisie == "+") {
+					string sa = stock.top();
+					stock.pop();
+					string sb = stock.top();
+					stock.pop();
+					float a = stof(sa);
+					float b = stof(sb);
+					Addition* adt = new Addition(b, a);
+					float c = adt->calculer();
+					string sc = to_string(c);
+					stock.push(sc);
+				}
+				else if (ma_saisie == "-") {
+					string sa = stock.top();
+					stock.pop();
+					string sb = stock.top();
+					stock.pop();
+					float a = stof(sa);
+					float b = stof(sb);
+					Soustraction* stc = new Soustraction(b, a);
+					float c = stc->calculer();
+					string sc = to_string(c);
+					stock.push(sc);
+				}
+				else if (ma_saisie == "*") {
+					string sa = stock.top();
+					stock.pop();
+					string sb = stock.top();
+					stock.pop();
+					float a = stof(sa);
+					float b = stof(sb);
+					Multiplication* mct = new Multiplication(b, a);
+					float c = mct->calculer();
+					string sc = to_string(c);
+					stock.push(sc);
+				}
+				else if (ma_saisie == "/") {
+					string sa = stock.top();
+					stock.pop();
+					string sb = stock.top();
+					stock.pop();
+					float a = stof(sa);
+					float b = stof(sb);
+					Division* dvt = new Division(b, a);
+					float c = dvt->calculer();
+					string sc = to_string(c);
+					stock.push(sc);
+				}
+				else {
+					cout << "ERROR" << endl;
+				}
+			}
+		}
 
-	// 	cout << "Le resultat de l'operation est : " << stock.top() << endl;
+		cout << "Le resultat de l'operation est : " << stock.top() << endl;
 }
